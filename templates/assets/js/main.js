e28c60f060b09f6b820867642eca5bfc35eb657a
liuzhihang <liuzhihangs@qq.com>
2022-10-21 21:13:08 +0800

    调整 logo，调整 js

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -82,14 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
-    function A() {
-        var a, c, r, s, l, d, u = document.getElementById("rightside"), f = window.innerHeight + 56;
-        document.body.scrollHeight <= f ? u.style.cssText = "opacity: 1; transform: translateX(-38px)" : (c = !(a = 0), r = document.getElementById("page-header"), document.getElementById("guli_top"), s = document.getElementById("cookies-window"), l = "function" == typeof chatBtnHide, d = "function" == typeof chatBtnShow, window.addEventListener("scroll", btf.throttle(function (t) {
-            var e, n, o = window.scrollY || document.documentElement.scrollTop, i = (n = a < (e = o), a = e, n);
-            56 < o ? (i ? (r.classList.contains("nav-visible") && r.classList.remove("nav-visible"), d && !0 === c && (chatBtnHide(), c = !1)) : (r.classList.contains("nav-visible") || r.classList.add("nav-visible"), l && !1 === c && (chatBtnShow(), c = !0)), r.classList.add("nav-fixed"),"0" === window.getComputedStyle(u).getPropertyValue("opacity") && (u.style.cssText = "opacity: 1; transform: translateX(-38px)")) : (0 === o && r.classList.remove("nav-fixed", "nav-visible"), u.style.cssText = "opacity: ''; transform: ''"), document.body.scrollHeight <= f && (u.style.cssText = "opacity: 1; transform: translateX(-38px)")
-        }, 200)))
-    }
-
     function S() {
         var t = document.getElementById("card-toc"), r = t.getElementsByClassName("toc-content")[0],
             s = r.querySelectorAll(".toc-link"), c = document.getElementById("article-container");
@@ -104,8 +96,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 o = (t - c.offsetTop) / (n < e ? e - n : document.documentElement.scrollHeight - n),
                 i = Math.round(100 * o), a = 100 < i ? 100 : i <= 0 ? 0 : i;
             r.setAttribute("progress-percentage", a)
-        }, l = GLOBAL_CONFIG.isanchor, n = function () {
-            t.style.cssText = "animation: toc-open .3s; opacity: 1; right: 45px"
         }, o = function () {
             t.style.animation = "toc-close .2s", setTimeout(function () {
                 t.style.cssText = "opacity:''; animation: ''; right: ''"
@@ -125,8 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 n > btf.getEleTop(t) - 80 && (o = "#" + encodeURI(t.getAttribute("id")), i = e)
             }), u !== i) {
                 if (l && (t = o, window.history.replaceState && t !== window.location.hash && (t = t || location.pathname, e = GLOBAL_CONFIG_SITE.title, window.history.replaceState({
-                    url: location.href,
-                    title: e
+                    url: location.href, title: e
                 }, e, t))), "" === o) return r.querySelectorAll(".active").forEach(function (t) {
                     t.classList.remove("active")
                 }), void (u = i);
@@ -143,60 +132,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    var d = !1, e = function () {
-        var e = document.body;
-        e.classList.add("read-mode");
-        var n = document.createElement("button");
-        n.type = "button", n.className = "fas fa-sign-out-alt exit-readmode", e.appendChild(n), n.addEventListener("click", function t() {
-            e.classList.remove("read-mode"), n.remove(), n.removeEventListener("click", t)
-        })
-    }, n = function () {
+    var d = !1, n = function () {
         "light" == ("dark" === document.documentElement.getAttribute("data-theme") ? "dark" : "light") ? (activateDarkMode(), saveToLocal.set("theme", "dark", 2), void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night, !1, 2e3)) : (activateLightMode(), saveToLocal.set("theme", "light", 2), void 0 !== GLOBAL_CONFIG.Snackbar && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day, !1, 2e3)), "function" == typeof utterancesTheme && utterancesTheme(), "object" === ("undefined" == typeof FB ? "undefined" : _typeof(FB)) && window.loadFBComment(), window.DISQUS && document.getElementById("disqus_thread").children.length && setTimeout(function () {
             return window.disqusReset()
         }, 200)
-    }, u = function () {
-        document.getElementById("rightside-config-hide").classList.toggle("show")
-    }, f = function () {
-        btf.scrollToDest(0, 500)
-    }, m = function () {
-        var t = document.documentElement.classList;
-        t.contains("hide-aside") ? saveToLocal.set("aside-status", "show", 2) : saveToLocal.set("aside-status", "hide", 2), t.toggle("hide-aside")
-    }, h = function (t) {
-        var e = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--global-font-size")),
-            n = "";
-        if (t) {
-            if (20 <= e) return;
-            n = e + 1, document.documentElement.style.setProperty("--global-font-size", n + "px"), document.getElementById("nav").classList.contains("hide-menu") || L(!0)
-        } else {
-            if (e <= 10) return;
-            n = e - 1, document.documentElement.style.setProperty("--global-font-size", n + "px"), document.getElementById("nav").classList.contains("hide-menu") && L(!0)
-        }
-        saveToLocal.set("global-font-size", n, 2)
     };
-    document.getElementById("rightside").addEventListener("click", function (t) {
-        switch (t.target.id || t.target.parentNode.id) {
-            case"go-up":
-                f();
-                break;
-            case"rightside_config":
-                u();
-                break;
-            case"readmode":
-                e();
-                break;
-            case"darkmode":
-                n();
-                break;
-            case"hide-aside-btn":
-                m();
-                break;
-            case"font-plus":
-                h(!0);
-                break;
-            case"font-minus":
-                h()
-        }
-    });
 
     function I(t) {
         t.forEach(function (t) {
@@ -237,46 +177,56 @@ document.addEventListener("DOMContentLoaded", function () {
             })
         })), l = document.getElementById("toggle-menu"), d = document.getElementById("sidebar-menus"), u = document.getElementById("menu-mask"), f = document.body, l.addEventListener("click", function () {
             btf.sidebarPaddingR(), f.style.overflow = "hidden", btf.fadeIn(u, .5), d.classList.add("open")
-        }), window.addEventListener("resize", function (t) {
+        });
+        window.addEventListener("resize", function (t) {
             btf.isHidden(l) && v()
-        }), GLOBAL_CONFIG.isPhotoFigcaption && document.querySelectorAll("#article-container img").forEach(function (t) {
-            var e, n = t.parentNode;
-            n.parentNode.classList.contains("justified-gallery") || ((e = document.createElement("div")).className = "img-alt is-center", e.textContent = t.getAttribute("alt"), n.insertBefore(e, t.nextSibling))
-        }), w(), "mediumZoom" === GLOBAL_CONFIG.lightbox && (h = mediumZoom(document.querySelectorAll("#article-container :not(a)>img"))).on("open", function (t) {
+        });
+
+        w();
+
+        "mediumZoom" === GLOBAL_CONFIG.lightbox && (h = mediumZoom(document.querySelectorAll("#article-container :not(a)>img"))).on("open", function (t) {
             var e = "dark" === document.documentElement.getAttribute("data-theme") ? "#121212" : "#fff";
             h.update({background: e})
-        }), A(), (g = document.querySelectorAll("#article-container :not(.highlight) > table, #article-container > table")).length && g.forEach(function (t) {
+        });
+
+        (g = document.querySelectorAll("#article-container :not(.highlight) > table, #article-container > table")).length && g.forEach(function (t) {
             btf.wrap(t, "div", "", "table-wrap")
-        }), (y = document.querySelectorAll("#article-container .hide-button")).length && y.forEach(function (t) {
+        });
+
+        (y = document.querySelectorAll("#article-container .hide-button")).length && y.forEach(function (t) {
             t.addEventListener("click", function (t) {
                 var e = this.nextElementSibling;
                 this.classList.toggle("open"), this.classList.contains("open") && 0 < e.querySelectorAll(".justified-gallery").length && btf.initJustifiedGallery(e.querySelectorAll(".justified-gallery"))
             })
-        }), O(), G(), p = !1, (b = document.querySelector("#comment-switch > .switch-btn")) && b.addEventListener("click", function () {
+        });
+        O();
+        G();
+        p = !1;
+        (b = document.querySelector("#comment-switch > .switch-btn")) && b.addEventListener("click", function () {
             this.classList.toggle("move"), document.querySelectorAll("#post-comment > .comment-wrap > div").forEach(function (t) {
                 btf.isHidden(t) ? t.style.cssText = "display: block;animation: tabshow .5s" : t.style.cssText = "display: none;animation: ''"
-            }), p || "function" != typeof loadOtherComment || (p = !0, loadOtherComment())
-        })
-    }, refreshFn(), window.addEventListener("resize", L), window.addEventListener("orientationchange", function () {
+            });
+            p || "function" != typeof loadOtherComment || (p = !0, loadOtherComment())
+        });
+    };
+    refreshFn();
+
+
+    window.addEventListener("resize", L);
+    window.addEventListener("orientationchange", function () {
         setTimeout(L(!0), 100)
-    }), document.querySelectorAll("#sidebar-menus .expand").forEach(function (t) {
+    });
+
+    document.querySelectorAll("#sidebar-menus .expand").forEach(function (t) {
         t.addEventListener("click", function () {
             this.classList.toggle("hide");
             var t = this.parentNode.nextElementSibling;
             btf.isHidden(t) ? t.style.display = "block" : t.style.display = "none"
         })
-    }), window.addEventListener("touchmove", function (t) {
+    });
+    window.addEventListener("touchmove", function (t) {
         document.querySelectorAll("#nav .menus_item_child").forEach(function (t) {
             btf.isHidden(t) || (t.style.display = "none")
         })
-    }), GLOBAL_CONFIG.islazyload && (window.lazyLoadInstance = new LazyLoad({
-        elements_selector: "img",
-        threshold: 0,
-        data_src: "lazy-src"
-    })), void 0 !== GLOBAL_CONFIG.copyright && (g = GLOBAL_CONFIG.copyright, document.body.oncopy = function (t) {
-        t.preventDefault();
-        var e = window.getSelection(0).toString(),
-            n = e.length > g.limitCount ? e + "\n\n\n" + g.languages.author + "\n" + g.languages.link + window.location.href + "\n" + g.languages.source + "\n" + g.languages.info : e;
-        return t.clipboardData ? t.clipboardData.setData("text", n) : window.clipboardData.setData("text", n)
-    })
+    });
 });
