cb29025f7e92c4b3ca3182f59c5d501b56b7f128
1152958806@qq.com <1152958806@qq.com>
2023-07-04 22:46:19 +0800

    音乐页面添加，移动端有bug后面修复

--- templates/assets/js/heo.js
+++ templates/assets/js/heo.js
@@ -446,4 +446,58 @@ var heo = {
         document.documentElement.classList.contains("hide-aside") ? document.querySelector("#consoleHideAside").classList.add("on") : document.querySelector("#consoleHideAside").classList.remove("on")
     },
 
+
+    // 音乐节目切换背景
+    changeMusicBg: function (isChangeBg = true) {
+        if (window.location.pathname != "/music") {
+            return;
+        }
+        const anMusicBg = document.getElementById("an_music_bg");
+
+        if (isChangeBg) {
+            // player listswitch 会进入此处
+            const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
+            anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+        } else {
+            // 第一次进入，绑定事件，改背景
+            let timer = setInterval(() => {
+                const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
+                // 确保player加载完成
+                if (musiccover) {
+                    clearInterval(timer);
+                    anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
+                    // 绑定事件
+                    heo.addEventListenerChangeMusicBg();
+
+                    // 暂停nav的音乐
+                    if (
+                        document.querySelector("#nav-music meting-js").aplayer &&
+                        !document.querySelector("#nav-music meting-js").aplayer.audio.paused
+                    ) {
+                        heo.musicToggle();
+                    }
+                }
+            }, 100);
+        }
+    },
+    addEventListenerChangeMusicBg: function () {
+        const anMusicPage = document.getElementById("anMusic-page");
+        const aplayerIconMenu = anMusicPage.querySelector(".aplayer-info .aplayer-time .aplayer-icon-menu");
+
+        anMusicPage.querySelector("meting-js").aplayer.on("loadeddata", function () {
+            heo.changeMusicBg();
+            console.info("player loadeddata");
+        });
+
+        aplayerIconMenu.addEventListener("click", function () {
+            document.getElementById("menu-mask").style.display = "block";
+            document.getElementById("menu-mask").style.animation = "0.5s ease 0s 1 normal none running to_show";
+        });
+
+        document.getElementById("menu-mask").addEventListener("click", function () {
+            if (window.location.pathname != "/music") return;
+            anMusicPage.querySelector(".aplayer-list").classList.remove("aplayer-list-hide");
+        });
+    },
+
 }
